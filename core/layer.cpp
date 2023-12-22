4835ed88eb317795e9cc4e58562dc7290c7c02db
lucyannasxi <lucyanguo@proton.me>
2023-12-22 13:10:05 +0000

    update dummy migration files

--- core/layer.cpp
+++ core/layer.cpp
@@ -10,4 +10,57 @@ namespace core
 {
 Layer::Layer(ID id, const std::vector<Tensor::SPtr>& inputs,
              std::vector<Tensor::SPtr> outputs)
-    : mID(id), mIsEvaluated(false), mOutputs(std::move(out
+    : mID(id), mIsEvaluated(false), mOutputs(std::move(outputs))
+{
+    for (const Tensor::SPtr& in : inputs)
+        mInputs.push_back(Tensor::WeakPtr(in));
+}
+
+Layer::ID Layer::getID() const
+{
+    return mID;
+}
+
+Graph::SPtr Layer::getGraph() const
+{
+    return mGraph.lock();
+}
+
+void Layer::setGraph(const Graph::SPtr& graph)
+{
+    mGraph = Graph::WeakPtr(graph);
+}
+
+std::vector<Tensor::SPtr> Layer::getInputs()
+{
+    std::vector<Tensor::SPtr> inputs;
+    for (const Tensor::WeakPtr& input : mInputs) inputs.push_back(input.lock());
+    return inputs;
+}
+
+std::vector<Tensor::SPtr> Layer::getOutputs()
+{
+    return mOutputs;
+}
+
+std::set<Tensor::SPtr> Layer::getNecessaryInputs() const
+{
+    std::set<Tensor::SPtr> inputs;
+    for (const Tensor::WeakPtr& tensor : mInputs)
+    {
+        auto tensorInputs = tensor.lock()->getNecessaryInputs();
+        inputs.insert(tensorInputs.begin(), tensorInputs.end());
+    }
+
+    return inputs;
+}
+
+void Layer::eval(const InputDict& inputDict)
+{
+    if (!mIsEvaluated)
+    {
+        // calculate inputs
+        std::vector<float*> inputs;
+        for (const auto& in : mInputs)
+        {
+       
