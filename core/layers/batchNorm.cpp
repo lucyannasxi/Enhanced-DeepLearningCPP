a2aeb7e6312d09d5aac8380d8ee67009c1260510
lucyannasxi <lucyanguo@proton.me>
2023-12-23 19:30:16 +0000

    fixed pep8 issues

--- core/layers/batchNorm.cpp
+++ core/layers/batchNorm.cpp
@@ -11,4 +11,45 @@
 
 namespace graphdl
 {
-namespace 
+namespace core
+{
+namespace layers
+{
+namespace
+{
+#define EPS 10e-8
+
+size_t getBatchSize(const Tensor::SPtr& tensor, int numAxes)
+{
+    TensorShape shape = tensor->getShape();
+    size_t size = 1;
+    for (int i = 0; i < numAxes; ++i) size *= shape[i];
+    return size;
+}
+
+size_t getFeatureSize(const Tensor::SPtr& tensor, int numAxes)
+{
+    return tensor->getShape().getCount() / getBatchSize(tensor, numAxes);
+}
+
+}  // namespace
+
+void runBatchNormHost(const float* x, const float* alpha, const float* beta,
+                      float* y, float* mean, float* stddev, size_t size,
+                      size_t batchSize)
+{
+    size_t stride = size / batchSize;
+
+    for (int i = 0; i < stride; ++i)
+    {
+        float val = 0.;
+        for (int j = 0; j < batchSize; ++j) val += x[j * stride + i];
+        mean[i] = val / float(batchSize);
+    }
+
+    for (int i = 0; i < stride; ++i)
+    {
+        float val = 0.;
+        for (int j = 0; j < batchSize; ++j)
+        {
+            float f = x[j * stride + i] - mean
