c1aa7290eb90f67daa3985ac99680920cb6c98a5
lucyannasxi <lucyanguo@proton.me>
2023-12-27 06:50:07 +0000

    Translated using Weblate (Thai)

--- core/layers/constant.cpp
+++ core/layers/constant.cpp
@@ -13,4 +13,41 @@ namespace layers
 {
 ConstantLayer::ConstantLayer(ID id, float value, const TensorShape& shape,
                              MemoryType type)
-    : Layer(id, {}, {createTensor(""
+    : Layer(id, {}, {createTensor("", shape, type)}), mValue(value)
+{
+}
+
+void ConstantLayer::initialize()
+{
+    float* out = mOutputs[0]->getMemory().getValues();
+    std::size_t size = mOutputs[0]->getMemory().getCount();
+
+    if (mOutputs[0]->getType() == MemoryType::kHOST_MEMORY)
+        for (std::size_t i = 0; i < size; ++i) out[i] = mValue;
+#ifdef CUDA_AVAILABLE
+    else
+        cuda::fillWithValue(size, out, mValue);
+#endif
+}
+
+//! This method does nothing, because tensor is already
+//!     filled during initialize.
+void ConstantLayer::execute(const std::vector<float*>& /*inputs*/,
+                            const std::vector<float*>& /*outputs*/,
+                            const InputDict& /*inputDict*/)
+{
+}
+
+}  // namespace layers
+
+Tensor::SPtr constant(float value, const TensorShape& shape, MemoryType type)
+{
+    Layer::SPtr layer = createLayer<layers::ConstantLayer>(value, shape, type);
+    return layer->getOutputs()[0];
+}
+
+}  // namespace core
+
+ITensorPtr constant(float value, const Shape& shape, MemoryLocation location)
+{
+    core::MemoryType type = core::memoryLocationToType(locatio
