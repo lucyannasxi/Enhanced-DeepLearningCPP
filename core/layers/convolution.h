694f1ea33e64ce19b429babd7161e14a7d2af08b
lucyannasxi <lucyanguo@proton.me>
2023-12-28 14:38:20 +0000

    zram: use scnprintf() in attrs show() methods

--- core/layers/convolution.h
+++ core/layers/convolution.h
@@ -62,4 +62,22 @@ namespace cuda
 {
 void runConv2DDevice(const float* x, const float* k, float* y,
                      const int* params, PaddingType padding,
-          
+                     DataFormat dataFormat);
+
+void runConv2DGradientDevice(const float* x, const float* k, const float* yG,
+                             float* xG, float* kG, const int* info,
+                             PaddingType padding, DataFormat dataFormat);
+
+}  // namespace cuda
+#endif
+}  // namespace layers
+
+Tensor::SPtr convolution2D(const Tensor::SPtr& t, const Tensor::SPtr& kernel,
+                           const std::vector<int>& strides,
+                           layers::PaddingType padding,
+                           layers::DataFormat dataFormat);
+
+}  // namespace core
+}  // namespace graphdl
+
+#endif  // GRAPHDL_CORE_LAYERS_CONVOLUTION_H_
