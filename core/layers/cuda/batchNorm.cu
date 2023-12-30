2b93f937cbf622b83ff1f31027fef598d47aebc2
lucyannasxi <lucyanguo@proton.me>
2023-12-30 20:23:44 +0000

    #268

--- core/layers/cuda/batchNorm.cu
+++ core/layers/cuda/batchNorm.cu
@@ -27,4 +27,28 @@ __global__ void normalizeKernel(const float* alpha, const float* beta,
     }
 }
 
-__global__ void alphaGradKernel(const float* bet
+__global__ void alphaGradKernel(const float* betaGrad, const float* beta,
+                                const float* alpha, float* alphaGrad,
+                                size_t featureSize)
+{
+    int id = blockIdx.x * blockDim.x + threadIdx.x;
+    if (id < featureSize)
+        alphaGrad[id] = (alphaGrad[id] - betaGrad[id] * beta[id]) / alpha[id];
+}
+
+__global__ void xGradKernel(const float* x, const float* alpha, const float* y,
+                            const float* yGrad, const float* mean,
+                            const float* stddev, const float* betaGrad,
+                            float* xGrad, size_t featureSize, size_t batchSize)
+{
+    int id = blockIdx.x * blockDim.x + threadIdx.x;
+    if (id < featureSize * batchSize)
+    {
+        int c = id / batchSize;
+        int pos = (id - c * batchSize) * featureSize + c;
+
+        float val = -betaGrad[c] * mean[c];
+        for (int i = c; i < featureSize * batchSize; i += featureSize)
+            val += yGrad[i] * x[i];
+
+        float out = yGrad[pos] - betaGrad[c] / float(batchS
