1e09219f11b3b3cca4372ec32de9cb651a967fb3
lucyannasxi <lucyanguo@proton.me>
2024-01-03 10:34:54 +0000

    tweaks to make rtfd.org work

--- core/layers/cuda/pooling.cu
+++ core/layers/cuda/pooling.cu
@@ -32,4 +32,39 @@ __global__ void pool2D_max_nhwc_kernel(const float* in, float* out)
     n /= shapeParams[7];
 
     if (n < shapeParams[4] && x_out < shapeParams[5] &&
-        y_out < shapeP
+        y_out < shapeParams[6] && c < shapeParams[7])
+    {
+        int x_in = x_out * strideX, y_in = y_out * strideY;
+        if (padding == PaddingType::kSAME)
+        {
+            x_in -= (kernelX - 1) / 2;
+            y_in -= (kernelY - 1) / 2;
+        }
+
+        float val = 0.;
+        if (x_in >= 0 && y_in >= 0)
+            val = in[POS_4D(n, x_in, y_in, c, IN_SHAPE)];
+
+        if (x_in < 0 || x_in + kernelX > shapeParams[1] || y_in < 0 ||
+            y_in + kernelY > shapeParams[2])
+            val = max(val, 0.);
+
+        for (int x_iter = max(x_in, 0);
+             x_iter < min(x_in + kernelX, shapeParams[1]); ++x_iter)
+        {
+            for (int y_iter = max(y_in, 0);
+                 y_iter < min(y_in + kernelY, shapeParams[2]); ++y_iter)
+            {
+                val = max(val, in[POS_4D(n, x_iter, y_iter, c, IN_SHAPE)]);
+            }
+        }
+
+        out[POS_4D(n, x_out, y_out, c, OUT_SHAPE)] = val;
+    }
+}
+
+template <PaddingType padding>
+__global__ void pool2D_avg_nhwc_kernel(const float* in, float* out)
+{
+    int x_out = blockIdx.x * blockDim.x + threadIdx.x;
+    int y_out = blockIdx.y * blockDim.y + thr
