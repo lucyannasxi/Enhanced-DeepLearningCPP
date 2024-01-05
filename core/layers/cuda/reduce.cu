3f5dfebc11896822bcb5d6295453a0dbf99701b0
lucyannasxi <lucyanguo@proton.me>
2024-01-05 09:44:53 +0000

    cleaning up docs

--- core/layers/cuda/reduce.cu
+++ core/layers/cuda/reduce.cu
@@ -13,4 +13,34 @@ void runReduceBackDevice(const float* x, float* y, size_t outSize,
                          size_t reduceSize, ReduceType reduceType)
 {
     switch (reduceType)
-    
+    {
+    case ReduceType::kSUM:
+        reduce<ReduceOpCuda::kSUM>(x, y, outSize, reduceSize);
+        break;
+    case ReduceType::kMIN:
+        reduce<ReduceOpCuda::kMIN>(x, y, outSize, reduceSize);
+        break;
+    case ReduceType::kMAX:
+        reduce<ReduceOpCuda::kMAX>(x, y, outSize, reduceSize);
+        break;
+    }
+}
+
+void runReduceBackGradientDevice(const float* x, const float* y,
+                                 const float* yGrad, float* xGrad,
+                                 size_t outSize, size_t reduceSize,
+                                 ReduceType reduceType)
+{
+    switch (reduceType)
+    {
+    case ReduceType::kSUM:
+        reduceGradient<ReduceOpCuda::kSUM>(x, y, yGrad, xGrad, outSize,
+                                           reduceSize);
+        break;
+    case ReduceType::kMIN:
+        reduceGradient<ReduceOpCuda::kMIN>(x, y, yGrad, xGrad, outSize,
+                                           reduceSize);
+        break;
+    case ReduceType::kMAX:
+        reduceGradient<ReduceOpCuda::kMAX>(x, y, yGrad, xGrad, outSize,
+                       
