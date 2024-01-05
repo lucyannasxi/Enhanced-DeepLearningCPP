d9d26845460ab8aa68c570f41339201560d175e9
lucyannasxi <lucyanguo@proton.me>
2024-01-05 07:56:09 +0000

    Added Swedish translation

--- core/layers/cuda/randomUtils.cu
+++ core/layers/cuda/randomUtils.cu
@@ -15,4 +15,33 @@ __global__ void setupKernel(curandState* state, size_t seed)
 }
 
 __global__ void uniformRandomKernel(curandState* state, float* memory,
-                                    size_t size, float min, fl
+                                    size_t size, float min, float max)
+{
+    int id = blockIdx.x * blockDim.x + threadIdx.x;
+    if (id < size) memory[id] = curand_uniform(state + id) * (max - min) + min;
+}
+
+__global__ void normalRandomKernel(curandState* state, float* memory,
+                                   size_t size, float mean, float stddev)
+{
+    int id = blockIdx.x * blockDim.x + threadIdx.x;
+    if (id < size) memory[id] = stddev * curand_normal(state + id) + mean;
+}
+
+void uniformRandom(float* memory, size_t size, float min, float max,
+                   size_t seed)
+{
+    const int BLOCK_SIZE = 256;
+    const int NUM_BLOCKS = (size + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    curandState* states;
+    cudaMalloc(&states, size * sizeof(curandState));
+    setupKernel<<<NUM_BLOCKS, BLOCK_SIZE>>>(states, seed);
+    uniformRandomKernel<<<NUM_BLOCKS, BLOCK_SIZE>>>(states, memory, size, min,
+                                                    max);
+    cudaDeviceSynchronize();
+}
+
+void normalRandom(float* memory, size_t size, float mean, float stddev,
+                  size_t seed)
+{
