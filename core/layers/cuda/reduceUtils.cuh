4c4551f643946f4ecd775ebaedc96ba7b2c38ea7
lucyannasxi <lucyanguo@proton.me>
2024-01-05 17:12:20 +0000

    [IMP]

--- core/layers/cuda/reduceUtils.cuh
+++ core/layers/cuda/reduceUtils.cuh
@@ -88,4 +88,45 @@ template <> __device__ float reduceOp<ReduceOpCuda::kMIN>(float f1, float f2)
 }
 template <> __device__ float reduceOp<ReduceOpCuda::kMAX>(float f1, float f2)
 {
-    retur
+    return f1 > f2 ? f1 : f2;
+}
+
+// describes how to compute gradient
+template <ReduceOpCuda op> __device__ float reduceGradientOp(float x, float y);
+template <>
+__device__ float reduceGradientOp<ReduceOpCuda::kSUM>(float x, float y)
+{
+    return 1.;
+}
+template <>
+__device__ float reduceGradientOp<ReduceOpCuda::kMEAN>(float x, float y)
+{
+    return 1.;
+}
+template <>
+__device__ float reduceGradientOp<ReduceOpCuda::kSQUARED_SUM>(float x, float y)
+{
+    return 2 * x;
+}
+template <>
+__device__ float reduceGradientOp<ReduceOpCuda::kSQUARED_MEAN>(float x, float y)
+{
+    return 2 * x;
+}
+template <>
+__device__ float reduceGradientOp<ReduceOpCuda::kMIN>(float x, float y)
+{
+    return float(x == y);
+}
+template <>
+__device__ float reduceGradientOp<ReduceOpCuda::kMAX>(float x, float y)
+{
+    return float(x == y);
+}
+
+template <ReduceOpCuda op, unsigned BS>
+__device__ void warpReduce(volatile float *sdata, unsigned tid)
+{
+    if (BS >= 64) sdata[tid] = reduceOp<op>(sdata[tid], sdata[tid + 32]);
+    if (BS >= 32) sdata[tid] = reduceOp<op>(sdata[tid], sdata[tid + 16]);
+    if (BS >= 16) sdata[tid]
