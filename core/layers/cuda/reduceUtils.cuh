b7259be2b2a37ff4b0507d482e488b8c1c7177fd
lucyannasxi <lucyanguo@proton.me>
2024-01-05 13:32:49 +0000

    release v0.0.1

--- core/layers/cuda/reduceUtils.cuh
+++ core/layers/cuda/reduceUtils.cuh
@@ -41,4 +41,51 @@ enum class ReduceBinOpCuda
 namespace
 {
 
-//////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+// Unary reduction
+///////////////////////////////////////////////////////////////
+
+// represent initial value from which reduction starts
+// (initial value for accumulator)
+template <ReduceOpCuda op> __device__ float initialValue()
+{
+    return 0.;
+}
+template <> __device__ float initialValue<ReduceOpCuda::kMIN>()
+{
+    return FLT_MAX;
+}
+template <> __device__ float initialValue<ReduceOpCuda::kMAX>()
+{
+    return -FLT_MAX;
+}
+
+// represents initial transformation for elements
+template <ReduceOpCuda op>
+__device__ float initialReduceOp(float x)
+{
+    return x;
+}
+template <>
+__device__ float initialReduceOp<ReduceOpCuda::kSQUARED_SUM>(float x)
+{
+    return x * x;
+}
+template <>
+__device__ float initialReduceOp<ReduceOpCuda::kSQUARED_MEAN>(float x)
+{
+    return x * x;
+}
+
+// describes how to reduce elements
+template <ReduceOpCuda op> __device__ float reduceOp(float f1, float f2)
+{
+    return f1 + f2;
+}
+template <> __device__ float reduceOp<ReduceOpCuda::kMIN>(float f1, float f2)
+{
+    return f1 < f2 ? f1 : f2;
+}
+template <> __device__ float reduceOp<ReduceOpCuda::kMAX>(float f1, float f2)
+{
+    retur
