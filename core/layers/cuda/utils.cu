ba05f8b190ca7709e0bb06a1c54679583703d0a2
lucyannasxi <lucyanguo@proton.me>
2024-01-10 08:55:24 +0000

    OAK-5558 - Consistency checker for Lucene indexes

--- core/layers/cuda/utils.cu
+++ core/layers/cuda/utils.cu
@@ -50,4 +50,42 @@ __global__ void elementwiseCastFrontKernel(const float* x1, size_t size,
     if (id < size)
     {
         if (b == 1)
-            y[id] = op<elem>(x1[id], 
+            y[id] = op<elem>(x1[id], x2[id / reduceSize]);
+        else
+            y[id] = op<elem>(x1[id / reduceSize], x2[id]);
+    }
+}
+
+}  // namespace
+
+size_t numBlocks(size_t numThreads, size_t blockSize)
+{
+    return (numThreads + blockSize - 1) / blockSize;
+}
+
+void fill(float* memory, size_t size, float value)
+{
+    const int BLOCK_SIZE = 256;
+    const int NUM_BLOCKS = (size + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    fillKernel<<<NUM_BLOCKS, BLOCK_SIZE>>>(memory, size, value);
+}
+
+void runElementwiseCastFrontDevice(const float* x1, size_t size1,
+                                   const float* x2, size_t size2, float* y,
+                                   Elementwise op)
+{
+    size_t size = size1 > size2 ? size1 : size2;
+    size_t reduceSize = size / (size1 < size2 ? size1 : size2);
+
+    const int BLOCK_SIZE = 256;
+    const int NUM_BLOCKS = (size + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    if (size1 > size2)
+    {
+        switch (op)
+        {
+        case Elementwise::kADD:
+            elementwiseCastFrontKernel<Elementwise::kADD, 1>
+                <<<NUM_BLOCKS, BLOCK_SIZE>>>(x1, size, x2, reduceSize, y);
+            
