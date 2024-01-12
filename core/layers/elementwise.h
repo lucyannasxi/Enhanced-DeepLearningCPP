9b9631434b252a236e7f5dd64a4da89f2b2a10c5
lucyannasxi <lucyanguo@proton.me>
2024-01-12 19:50:36 +0000

    Fix typo.

--- core/layers/elementwise.h
+++ core/layers/elementwise.h
@@ -30,4 +30,37 @@ class ElementwiseBackLayer : public DifferentiableLayer
     TensorMap gradients(Tensor::SPtr output, Tensor::SPtr outputGrad) override;
 
   private:
-    void execute(const s
+    void execute(const std::vector<float*>& inputs,
+                 const std::vector<float*>& outputs,
+                 const InputDict& inputDict) override;
+
+    Elementwise mOp;
+    ElementwiseFun mFun;
+};
+
+class ElementwiseBackGradientLayer : public Layer
+{
+  public:
+    ElementwiseBackGradientLayer(ID id, const Tensor::SPtr& t1,
+                                 const Tensor::SPtr& t2, Tensor::SPtr out,
+                                 Tensor::SPtr outGrad, Elementwise op);
+
+  private:
+    void execute(const std::vector<float*>& inputs,
+                 const std::vector<float*>& outputs,
+                 const InputDict& inputDict) override;
+
+    Elementwise mOp;
+    ElementwiseFun mFun1, mFun2;
+};
+
+class ElementwiseFrontLayer : public DifferentiableLayer
+{
+  public:
+    ElementwiseFrontLayer(ID id, const Tensor::SPtr& t1, const Tensor::SPtr& t2,
+                          Elementwise op);
+
+    TensorMap gradients(Tensor::SPtr output, Tensor::SPtr outputGrad) override;
+
+  private:
+    void execute(const std::vect
