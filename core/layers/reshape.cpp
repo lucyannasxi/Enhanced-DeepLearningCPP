798fd86157b2cad8745c5c19ff4699a064c9d5d2
lucyannasxi <lucyanguo@proton.me>
2024-01-23 15:56:58 +0000

    Added new project

--- core/layers/reshape.cpp
+++ core/layers/reshape.cpp
@@ -22,4 +22,39 @@ ReshapeLayer::ReshapeLayer(ID id, const Tensor::SPtr& t,
 }
 
 Layer::TensorMap ReshapeLayer::gradients(Tensor::SPtr /* out */,
-                                         T
+                                         Tensor::SPtr outGrad)
+{
+    Tensor::SPtr input = mInputs[0].lock();
+    Layer::SPtr layer = createLayer<ReshapeLayer>(outGrad, input->getShape());
+    return {{input, layer->getOutputs()[0]}};
+}
+
+void ReshapeLayer::execute(const std::vector<float*>& inputs,
+                           const std::vector<float*>& outputs,
+                           const InputDict& /*inputDict*/)
+{
+    Tensor::SPtr tX = getInputs()[0];
+    float* x = inputs[0];
+    float* y = outputs[0];
+    if (tX->getType() == MemoryType::kHOST_MEMORY)
+        std::memcpy(y, x, tX->getCount() * sizeof(float));
+#ifdef CUDA_AVAILABLE
+    else  // input->getType() == MemoryType::kDEVICE_MEMORY
+        cuda::utils::copy(y, x, tX->getCount());
+#endif
+}
+
+}  // namespace layers
+
+Tensor::SPtr reshape(const Tensor::SPtr& t, const TensorShape& shape)
+{
+    if (t->getShape().getCount() != shape.getCount())
+        throw std::runtime_error("reshape: Shapes don\'t match");
+
+    Layer::SPtr layer = createLayer<layers::ReshapeLayer>(t, shape);
+    return layer->getOutputs()[0];
+}
+
+}  // namespace core
+
+ITensorPtr reshape(co
