b33de3d4f1564483eace639ded73ea067edb43c7
lucyannasxi <lucyanguo@proton.me>
2024-01-26 19:28:05 +0000

    Fix import error.

--- core/trainers/momentum.cpp
+++ core/trainers/momentum.cpp
@@ -36,3 +36,43 @@ Tensor::SPtr MomentumTrainer::parseGradients(
         steps.insert({w, wStep});
     }
 
+    std::vector<Tensor::SPtr> stepUpdates;
+    stepUpdates.reserve(grads.size());
+    for (const auto& grad : grads)
+    {
+        Tensor::SPtr w = grad.first;
+        Tensor::SPtr g = grad.second;
+        Tensor::SPtr s = steps[w];
+
+        Tensor::SPtr a = assign(s, mMomentum * s + mLearningRate * g);
+        stepUpdates.push_back(a);
+    }
+
+    std::vector<Tensor::SPtr> weightsUpdates;
+    weightsUpdates.reserve(grads.size());
+    for (const auto& step : steps)
+    {
+        Tensor::SPtr w = step.first;
+        Tensor::SPtr s = step.second;
+
+        Tensor::SPtr a = assign(w, w - s);
+        weightsUpdates.push_back(a);
+    }
+
+    return queue({group(stepUpdates), group(weightsUpdates)});
+}
+
+}  // namespace trainers
+}  // namespace core
+
+namespace train
+{
+ITrainerPtr momentum(float lr, float m)
+{
+    core::Trainer::UPtr t =
+        std::make_unique<core::trainers::MomentumTrainer>(lr, m);
+    return core::makeAbstractTrainer(std::move(t));
+}
+
+}  // namespace train
+}  // namespace graphdl
