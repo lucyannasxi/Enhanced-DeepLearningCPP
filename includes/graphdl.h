164de328c26728392767945824cc031e533cab6a
lucyannasxi <lucyanguo@proton.me>
2024-01-27 18:16:02 +0000

    Plural

--- includes/graphdl.h
+++ includes/graphdl.h
@@ -30,4 +30,57 @@ using InputDict = std::map<std::string, HostTensor>;
 
 //! \typedef Shape
 //! \brief Represents shape of the tensor.
-/
+//!
+using Shape = std::vector<unsigned int>;
+
+//! \enum MemoryLocation
+//! \brief Represents type of memory.
+//!
+enum class MemoryLocation
+{
+    ///< memory on host (CPU)
+    kHOST = 0,
+    ///< memory on device (GPU)
+    kDEVICE = 1,
+    ///< use memory on device if availble, host otherwise
+    kDEVICE_IF_ENABLED = 2,
+};
+
+//! \brief Metatype for shared pointers.
+//!
+template <typename T>
+using SharedPtr = std::shared_ptr<T>;
+
+class ITensor;
+
+//! \typedef ITensorPtr
+//! \brief Shared pointer to ITensor.
+//!
+using ITensorPtr = SharedPtr<ITensor>;
+
+class IGraph;
+
+//! \typedef IGraphPtr
+//! \brief Shared pointer to IGraph.
+//!
+using IGraphPtr = SharedPtr<IGraph>;
+
+//! \class ITensor
+//! \brief Interface representing tensor.
+//!
+class ITensor
+{
+  public:
+    //! \fn getName
+    //! \brief Returns name of the tensor.
+    //!
+    virtual std::string getName() const = 0;
+
+    //! \fn setName
+    //! \brief Sets new name for the tensor.
+    //!
+    virtual void setName(const std::string& name) = 0;
+
+    //! \fn getShape
+    //! \brief Returns shape of the tensor.
+    
