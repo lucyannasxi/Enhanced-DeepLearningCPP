587229435e00dcecf09f7d4b3af13c546b41dc0c
lucyannasxi <lucyanguo@proton.me>
2024-02-07 12:07:13 +0000

    usb: host: xhci: use new USB_RESUME_TIMEOUT

--- samples/cifar10_conv/main_cifar10_conv.cpp
+++ samples/cifar10_conv/main_cifar10_conv.cpp
@@ -23,4 +23,42 @@ const std::vector<std::string> TRAIN_PATHS = {
     QUOTE(PROJECT_DIR) + "/samples/cifar10_conv/data_batch_5.bin",
 };
 const std::vector<std::string> VALID_PATHS = {
-    QUOTE(PROJECT
+    QUOTE(PROJECT_DIR) + "/samples/cifar10_conv/test_batch.bin"};
+
+#undef Q
+#undef QUOTE
+
+using namespace graphdl;
+
+//! \fn conv2DAndMaxPool2D
+//! \brief Helper function that does conv2D -> pool2D -> relu.
+//!
+ITensorPtr conv2DAndMaxPool2D(const ITensorPtr& x, const ITensorPtr& k)
+{
+    ITensorPtr a = conv2D(x, k, {1, 1}, "SAME", "NHWC");
+    return relu(maxPool2D(a, {2, 2}, {2, 2}, "VALID", "NHWC"));
+}
+
+ComputationalGraph buildNetwork()
+{
+    MemoryLocation loc = MemoryLocation::kDEVICE_IF_ENABLED;
+
+    // inputs
+    ITensorPtr X = createInput("X", {BATCH_SIZE, 32, 32, 3}, loc);
+    ITensorPtr Y = createInput("Y", {BATCH_SIZE, 10}, loc);
+
+    ITensorPtr a = X;
+
+    a = create_conv2D(a, 8, {3, 3}, {1, 1}, "SAME", "NHWC", "conv1");
+    a = maxPool2D(a, {2, 2}, {2, 2}, "VALID", "NHWC");
+    a = relu(a);
+
+    a = create_conv2D(a, 16, {3, 3}, {1, 1}, "SAME", "NHWC", "conv2");
+    a = maxPool2D(a, {2, 2}, {2, 2}, "VALID", "NHWC");
+    a = relu(a);
+
+    a = create_conv2D(a, 32, {3, 3}, {1, 1}, "SAME", "NHWC", "conv3");
+    a = maxPool2D(a, {2, 2}, {2, 2}, "VALID", "NHWC");
+    a = relu(a);
+
+    a
