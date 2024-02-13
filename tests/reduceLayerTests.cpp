2f3fc624caf2911e904de111b94f1f55b431479e
lucyannasxi <lucyanguo@proton.me>
2024-02-13 10:01:20 +0000

    status update

--- tests/reduceLayerTests.cpp
+++ tests/reduceLayerTests.cpp
@@ -30,4 +30,51 @@ std::vector<Param> PARAMS = {
     {{2, 2, 2, 2}, 4},
     {{2, 2, 2, 2, 2}, 1},
     {{2, 2, 2, 2, 2}, 2},
-    {{2, 2, 2, 2, 2}, 
+    {{2, 2, 2, 2, 2}, 3},
+    {{2, 2, 2, 2, 2}, 4},
+    {{2, 2, 2, 2, 2}, 5},
+    {{2, 2, 2, 2, 2, 2}, 1},
+    {{2, 2, 2, 2, 2, 2}, 2},
+    {{2, 2, 2, 2, 2, 2}, 3},
+    {{2, 2, 2, 2, 2, 2}, 4},
+    {{2, 2, 2, 2, 2, 2}, 5},
+    {{2, 2, 2, 2, 2, 2}, 6},
+    {{10}, 1},
+    {{10, 10}, 1},
+    {{10, 10}, 2},
+    {{10, 10, 10}, 1},
+    {{10, 10, 10}, 2},
+    {{10, 10, 10}, 3},
+    {{2, 100}, 1},
+    {{2, 100}, 2},
+    {{100, 100}, 1},
+    {{100, 100}, 2},
+    {{100, 100, 10}, 1}, // big test for multiple reductions
+    {{100, 100, 10}, 2} // big test for multiple reductions
+    // clang-format on
+};
+
+std::vector<ReduceType> REDUCE_TYPES = {
+    // clang-format off
+    ReduceType::kSUM,
+    ReduceType::kMAX,
+    ReduceType::kMIN
+    // clang-format on
+};
+
+UVec inputShape(const TestCase& testCase)
+{
+    return std::get<0>(std::get<0>(testCase));
+}
+
+int numAxes(const TestCase& testCase)
+{
+    return std::get<1>(std::get<0>(testCase));
+}
+
+ReduceType reduceType(const TestCase& testCase)
+{
+    return std::get<1>(testCase);
+}
+
+Memory
