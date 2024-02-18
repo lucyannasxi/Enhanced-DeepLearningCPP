537f0bba1a531ab3f8b7cbbeac5d06c9148ab453
lucyannasxi <lucyanguo@proton.me>
2024-02-18 05:56:21 +0000

    Code added

--- tests/refTensor.cpp
+++ tests/refTensor.cpp
@@ -232,4 +232,48 @@ RefTensor RefTensor::slice(Coord start, const TensorShape& shape) const
 
     for (Coord_iterator it = tensor.begin(); it != tensor.end(); ++it)
     {
-        if (isInside(start 
+        if (isInside(start + it(), mShape))
+            tensor[it()] = this->operator[](start + it());
+        else
+            tensor[it()] = 0.;
+    }
+
+    return tensor;
+}
+
+std::ostream& operator<<(std::ostream& stream, const RefTensor& tensor)
+{
+    for (std::size_t i = 0; i < tensor.getCount(); ++i)
+        stream << tensor.at(i) << " ";
+    return stream;
+}
+
+std::ostream& operator<<(std::ostream& stream, const Coord& c)
+{
+    stream << "[";
+    for (int i = 0; i < c.size() - 1; ++i) stream << c[i] << ", ";
+    stream << c[c.size() - 1] << "]";
+    return stream;
+}
+
+Coord_iterator shapeBegin(const TensorShape& shape)
+{
+    std::vector<int> c, s;
+    for (unsigned i = 0; i < shape.size(); ++i)
+    {
+        c.push_back(0);
+        s.push_back(shape[i]);
+    }
+    return Coord_iterator(Coord(c), Coord(s));
+}
+
+Coord_iterator shapeEnd(const TensorShape& shape)
+{
+    std::vector<int> c, s;
+    for (unsigned i = 0; i < shape.size(); ++i)
+    {
+        c.push_back(0);
+        s.push_back(shape[i]);
+    }
+    c[0] = shape[0];
+    return Coord_iterator(Coord(c), C
